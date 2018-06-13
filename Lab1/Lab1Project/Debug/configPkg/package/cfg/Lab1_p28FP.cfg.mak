d26e28105cb058580e6fbfd894d47ace31bf4110
Hang <cuihang1201@gmail.com>
2018-06-12 20:02:05 -0500

    Add labs

--- Lab1/Lab1Project/Debug/configPkg/package/cfg/Lab1_p28FP.cfg.mak
+++
@@ -1,8 +0,0 @@
-# invoke SourceDir generated makefile for Lab1.p28FP
-Lab1.p28FP: .libraries,Lab1.p28FP
-.libraries,Lab1.p28FP: package/cfg/Lab1_p28FP.xdl
-	$(MAKE) -f C:\DSP_TI_TMS320F28377S\Lab1/src/makefile.libs
-
-clean::
-	$(MAKE) -f C:\DSP_TI_TMS320F28377S\Lab1/src/makefile.libs clean
-
--- Lab2/Lab2Project/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.1.LTS/lib/rts2800_fpu32.lib" -l"C:/DSP_TI_TMS320F28377S/Lab2/v140/F2837xS_common/cmd/2837x_Dan_FLASH_lnk_cpu1.cmd" -l"C:/DSP_TI_TMS320F28377S/Lab2/v140/F2837xS_headers/cmd/F2837xS_Headers_BIOS.cmd"
-
